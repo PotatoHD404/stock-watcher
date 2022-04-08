536c11d8df7f4f4ead59e6f198d262e829cd6641
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2022-04-08 17:36:25 +0300

    Added template project

---
+++ src/main/java/com/example/javahackathon/JavaHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.javahackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaHackathonApplication.class, args);
+    }
+
+}
